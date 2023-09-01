7e1bf3bf5f1de9952266709f6e46a87282040365
Alex <alex97av@gmail.com>
2023-09-01 15:02:22 +0200

    add hashCode

--- src/Fraction.java
+++ src/Fraction.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class Fraction {
+public class Fraction {
+    private int num;
+    private int den;
+
+
+    public Fraction(int num , int den){
+        this.den = den;
+        this.num = num;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        Fraction other = (Fraction) obj;
+        boolean frn = this.num == other.num && this.den == other.den;
+
+        return frn;
+    }
+
+    @Override
+    public int hashCode() {
+        return num*1000+den;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+
+        Fraction f1 = new Fraction( 4 , 5);
+        Fraction f2 = new Fraction(5 , 5);
+
+        System.out.println(f1.equals(f2));
+
+        System.out.println(f1 == f2);
+        System.out.println("F1 hashCode: "+f1.hashCode());
+        System.out.println("F2 hashCode: "+f2.hashCode());
+
+
+    }
 }
